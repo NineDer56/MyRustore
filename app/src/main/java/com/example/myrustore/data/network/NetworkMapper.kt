83d4058396e3ce8073aa7daf00e21dd1c7b4619b
Ilya <Ilya.boltyshev@mail.ru>
2025-11-04 20:10:20 +0300

    add NetworkMapperTest and update mapper

--- app/src/main/java/com/example/myrustore/data/network/NetworkMapper.kt
+++ app/src/main/java/com/example/myrustore/data/network/NetworkMapper.kt
@@ -9,24 +9,32 @@ class NetworkMapper @Inject constructor() {
     fun appItemDtoToEntity(dto: AppItemDto): AppItem {
         return AppItem(
             id = dto.id,
-            name = if (dto.name.isNullOrBlank()) "Неизвестно" else dto.name,
-            description = if (dto.description.isNullOrBlank()) "Отсутствует" else dto.description,
+            name = if (dto.name.isNullOrBlank()) NO_NAME else dto.name,
+            description = if (dto.description.isNullOrBlank()) NO_DESCRIPTION else dto.description,
             category = dto.category ?: AppCategory.APP,
-            iconUrl = dto.iconUrl ?: ""
+            iconUrl = if(dto.iconUrl.isNullOrBlank()) NO_ICON_URL else dto.iconUrl
         )
     }
 
     fun appDetailsDtoToEntity(dto: AppDetailsDto): AppDetails {
         return AppDetails(
             id = dto.id,
-            name = if (dto.name.isNullOrBlank()) "Неизвестно" else dto.name,
-            developer = if (dto.developer.isNullOrBlank()) "Отсутствует" else dto.developer,
+            name = if (dto.name.isNullOrBlank()) NO_NAME else dto.name,
+            developer = if (dto.developer.isNullOrBlank()) NO_DEVELOPER else dto.developer,
             category = dto.category ?: AppCategory.APP,
-            ageRating = dto.ageRating ?: 0,
+            ageRating = dto.ageRating ?: NO_AGE_RATING,
             size = dto.size,
-            iconUrl = dto.iconUrl ?: "",
+            iconUrl = if(dto.iconUrl.isNullOrBlank()) NO_ICON_URL else dto.iconUrl,
             screenshotUrlList = dto.screenshotUrlList?.toList() ?: emptyList(),
-            description = if (dto.description.isNullOrBlank()) "Отсутствует" else dto.description,
+            description = if (dto.description.isNullOrBlank()) NO_DESCRIPTION else dto.description,
         )
+    }
+
+    companion object {
+        const val NO_NAME = "Неизвестно"
+        const val NO_DEVELOPER = "Отсутствует"
+        const val NO_DESCRIPTION = "Отсутствует"
+        const val NO_ICON_URL = ""
+        const val NO_AGE_RATING = 0
     }
 }
