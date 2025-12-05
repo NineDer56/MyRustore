423a611724f6dc4b4ce1ce2ce655312e92e205cb
Ilya <Ilya.boltyshev@mail.ru>
2025-12-05 17:51:08 +0300

    clean code

--- app/src/main/java/com/example/myrustore/presentation/screens/appDetails/AppDetailsScreen.kt
+++ app/src/main/java/com/example/myrustore/presentation/screens/appDetails/AppDetailsScreen.kt
@@ -21,7 +21,6 @@ fun AppDetailsScreen(
     contentPadding : PaddingValues,
     id : String
 ){
-    // TODO нажатие на кнопку "перезагрузить" после появления интернета приводит к тому что скриншоты могут не загрузиться, нужно переделать dao
     val viewModel : AppDetailsViewModel = hiltViewModel()
     val state by viewModel.state.collectAsStateWithLifecycle()
     val downloadState by viewModel.downloadState.collectAsStateWithLifecycle()
